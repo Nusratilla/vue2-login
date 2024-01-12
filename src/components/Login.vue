b9c2076a6d950ab9defc6dea562db53e8e1a0d29
Nusrat <nusratilla.88@gmail.com>
2024-01-12 22:09:43 +0500

    a

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="form-signin w-50 m-auto">
     <form>
-      <h1 class="h3 mb-3 mt-5 fw-normal">Login</h1>
+      <h1 class="h3 mb-5 mt-5 fw-normal">Login</h1>
 
       <Input :label="'Email address'" :type="'email'"></Input>
       <Input :label="'Password'" :type="'password'"></Input>
@@ -14,8 +14,4 @@ export default {
 
 }
 </script>
-<style>
-/* .form-signin {
-  font-size: 1rem;
-} */
-</style>
+<style></style>
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="form-signin w-50 m-auto">
     <form>
-      <h1 class="h3 mb-3 mt-5 mb-5 fw-normal">Register</h1>
+      <h1 class="h3 mt-5 mb-5 fw-normal">Register</h1>
 
       <Input :label="'Name'" :type="'text'"></Input>
       <Input :label="'Email address'" :type="'email'"></Input>
