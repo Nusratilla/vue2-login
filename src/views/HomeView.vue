ae67d8c03e6a34f1a214187cc14f0c89b7645b95
Nusrat <nusratilla.88@gmail.com>
2024-01-25 23:06:39 +0500

    article card added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-    Home Section <br>
-    {{ data }}
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 mt-3">
+      <ArticleCard v-for="article in data" :article="article" :key="article.id" />
+
+    </div>
   </div>
 </template>
 <script>
 import { mapState } from 'vuex';
+import ArticleCard from '@/components/ArticleCard.vue'
 export default {
+  components: {
+    ArticleCard,
+  },
   computed: {
     ...mapState({
       data: state => state.articles.data,
