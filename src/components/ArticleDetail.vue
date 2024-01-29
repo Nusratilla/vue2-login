6d3763147c88124ce51f977f2142628dcf5f0cd8
Nusrat <nusratilla.88@gmail.com>
2024-01-29 22:53:09 +0500

    new component

---
+++ src/components/ArticleDetail.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    articledetail: {
+      type: Object,
+      requires: true,
+    }
+  }
+}
+</script>
+<style></style>
